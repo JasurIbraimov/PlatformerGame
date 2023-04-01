25ffa93166373b87a70b66177e08e65f658420fc
Jasur <ibraimov0409@mail.ru>
2023-04-01 07:44:30 +0600

    Finished Project

---
+++ button.py
@@ -0,0 +1,13 @@
+import arcade 
+
+class Button(arcade.Sprite):
+    def __init__(self, text, image, center_x, center_y):
+        super().__init__(image)
+        self.center_x = center_x 
+        self.center_y = center_y
+        self.restart_text = arcade.Text("RESTART", center_x - 60, center_y-5, arcade.color.GHOST_WHITE,15, 
+                    font_name="kenvector future")
+    
+    def draw(self): 
+        super().draw()
+        self.restart_text.draw()
---
+++ trap.py
@@ -0,0 +1,8 @@
+import arcade 
+
+class Trap(arcade.Sprite):
+    def __init__(self, image):
+        super().__init__(image, scale=0.75)
+    
+    def update(self):
+        self.angle += self.change_angle
